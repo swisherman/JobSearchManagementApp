c7e1f0004c0f36ffc8d723b43b7a6b3dd4e25f33
bobwa <bob.walkama@gmail.com>
2026-05-31 09:40:03 -0400

    Working on layout

--- Models/JobSearchDatabaseSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace JobSearchApp.Models;
-
-public class JobSearchDatabaseSettings
-{
-    public string ConnectionString { get; set; } = string.Empty;
-    public string DatabaseName { get; set; } = string.Empty;
-}
